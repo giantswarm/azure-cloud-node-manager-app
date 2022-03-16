06affd60a24cbfe46674bcb848ce30c6299c747f
Christian Bianchi <whites11@gmail.com>
2022-03-16 16:13:57 +0100

    removed integration tests

--- integration/templates/azure_cnm_values.go
+++
@@ -1,9 +0,0 @@
-//go:build k8srequired
-// +build k8srequired
-
-package templates
-
-// AzureCloudNodeManagerValues values required by azure-cloud-node-manager-app.
-const AzureCloudNodeManagerValues = `
-name: azure-cloud-node-manager-test
-`
